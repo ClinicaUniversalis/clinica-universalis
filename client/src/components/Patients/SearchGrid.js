94422031e1a732ea9af1f4ca7f8df802ab831384
Marco Glz <marcogonzalezguzman77@gmail.com>
2022-05-12 16:24:32 -0500

    Added elements to Search and PatientsGrid -DM

--- client/src/components/Patients/SearchGrid.js
+++ client/src/components/Patients/SearchGrid.js
@@ -2,19 +2,20 @@ import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react'
 import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
 import 'ag-grid-community/dist/styles/ag-grid.css'; // Core grid CSS, always needed
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css'; // Optional theme CSS
-import { Row, NavLink, Button, Tab, Container } from 'react-bootstrap';
+import { Row, NavLink, Button, Tab, Container, Form } from 'react-bootstrap';
 import { BsPersonPlusFill } from 'react-icons/bs';
 import { render } from 'react-dom';
-
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import Spinner from '../../components/Spinner/Spinner';
 import CreateEditPatient from './CreateEditPatient';
+//import { FilterComponent } from 'ag-grid-community/dist/lib/components/framework/componentTypes';
 
 const SearchGrid = () => {
     const gridRef = useRef();
     const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
     const [showLimitedPatientInfoModal, setShowLimitedPatientInfoModal] = useState(false);
+    const [patientSearchName, setPatientSearchName] = useState("");
+    const [patientSearchID, setPatientSearchID] = useState("");
+    const [patientSearchBD, setPatientSearchBD] = useState("");
 
     const handleShowLimitedPatientInfoModalOpen = () => {
         showLimitedPatientInfoModal(true);
@@ -23,10 +24,26 @@ const SearchGrid = () => {
     const handleCreatePatientModalClose = () => {
         showLimitedPatientInfoModal(false);
     }
+    const handleInputChange = (event) =>{
+        const { target } = event;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if(inputType == "patientSearchName"){
+            setPatientSearchName(inputValue);
+        }
+
+        if(inputType == "patientSearchID"){
+            setPatientSearchID(inputValue);
+        }
+
+        if(inputType == "patientSearchBD"){
+            setPatientSearchBD(inputValue);
+        }
+    }
 
     // Column Definition results in one Column.
     const [columnDefs, setColumnDefs] = useState([
-        { field: 'delete', filter: true},
         { field: 'full name', filter: true},
         { field: 'email', filter: true },
         { field: 'phone', filter: true },
@@ -56,6 +73,34 @@ const SearchGrid = () => {
 
     return (
         <>
+        <Form>
+                    <Form.Group controlId="patientSearchNameInput" style={{margin:"3em"}}>
+                        <Form.Label>Search by name: </Form.Label>
+                        <Form.Control 
+                            size='lg'
+                            type="patientSearchName"
+                            placeholder="Search patient"
+                            name="patientSearchName"
+                            value={patientSearchName}
+                            onChange={handleInputChange}/>
+                    <Form.Label>Search ID: </Form.Label>
+                        <Form.Control 
+                            size='lg'
+                            type="patientSearchID"
+                            placeholder="Search ID"
+                            name="patientSearchID"
+                            value={patientSearchID}
+                            onChange={handleInputChange}/>
+                    <Form.Label>Search by Birth Date: </Form.Label>
+                        <Form.Control 
+                            size='lg'
+                            type="patientSearchBD"
+                            placeholder="Search BD"
+                            name="patientSearchBD"
+                            value={patientSearchBD}
+                            onChange={handleInputChange}/>
+                    </Form.Group>
+                </Form>
             <Row>
                 
             </Row>
