808cb519a4df32310f74184e0b832ebde22dca2c
Guillermo Lazo <glazovg@gmail.com>
2022-05-12 20:05:38 -0500

    Heroku mongoDb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/clinicauniversalis',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/clinicauniversalisdb',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
